8cb8306206bfd758cfaea55c1be621311a721cdb
wookyung104 <86592237+wookyung104@users.noreply.github.com>
2021-07-01 16:51:09 +0900

    Add files via upload

---
+++ ar.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>web cam connection test</title>
+        <script>
+            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                    var video = document.getElementById('video');
+                    video.srcObject = stream;
+                    video.play();
+                });
+            }
+            var video = document.getElementById('video');
+        </script>
+
+
+        <style>
+            .video_absolute
+            {
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                z-index: 2;                
+            }
+        </style>
+    </head>
+    <body>
+        <video id="video" width="100%" height="100%" controls="true" autoplay></video>
+        <?php
+            $x_pos = 20;
+            $y_pos = 15;
+            echo "<img style=\"position:absolute; top:",$y_pos,"%",";left:",$x_pos,"%;\"src=\"./오구.gif\"></img>";
+            echo "<img style=\"position:absolute; top:",$x_pos,"%",";right:",$y_pos,"%;\"src=\"./nyan-cat.gif\"></img>";
+        ?>
+
+    </body>
+</html>
